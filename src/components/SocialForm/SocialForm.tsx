d2a897ed81d648b1abc38db91b78c74d152b6cda
Roman Chaban <roman.chaban1001@gmail.com>
2024-05-19 22:48:55 +0300

    Fixed Button component for SocialForm component

--- src/components/SocialForm/SocialForm.tsx
+++ src/components/SocialForm/SocialForm.tsx
@@ -4,28 +4,29 @@ import { FC } from 'react';
 import styles from './socialForm.module.scss';
 import { Button } from '../ui/Button/Button';
 import { Facebook, Google, Phone } from 'grommet-icons';
+import { ButtonBase } from '@mui/material';
 
 export const SocialForm: FC = () => {
   return (
     <div className={styles.socialContainer}>
       <h5 className={styles.social__title}>Or</h5>
       <div className={styles.social__buttons}>
-        <Button type='button' id={styles.social} className={styles.withGoogle}>
+        <ButtonBase type='button' id={styles.social} className={styles.withGoogle}>
           <Google color='plain' />
           Sign Up with Google
-        </Button>
-        <Button
+        </ButtonBase>
+        <ButtonBase
           type='button'
           id={styles.social}
           className={styles.withFacebook}
         >
           <Facebook color='plain' />
           Sign Up with Facebook
-        </Button>
-        <Button type='button' id={styles.social} className={styles.withPhone}>
+        </ButtonBase>
+        <ButtonBase type='button' id={styles.social} className={styles.withPhone}>
           <Phone color='#111111' />
           Sign Up with Number
-        </Button>
+        </ButtonBase>
       </div>
     </div>
   );
