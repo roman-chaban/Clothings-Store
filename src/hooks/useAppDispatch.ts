1c81906f14abf190ce29e8ab9be9997792d4e959
Roman Chaban <roman.chaban1001@gmail.com>
2024-05-28 03:56:14 +0300

    Added new imports for reduxHooks useAppSelector/useAppDispatch

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -1,4 +1,4 @@
-import { AppDispatch } from '@/redux/store/store';
+import { AppDispatch } from '@/store/store/store';
 import { useDispatch } from 'react-redux';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,4 +1,4 @@
-import { RootState } from '@/redux/rootReducer/rootReducer';
+import { RootState } from '@/store/rootReducer/rootReducer';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
